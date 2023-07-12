384e4e82dc038256e04f26c044bdac2bcd8894d0
Serdar Mengütay <serdarmngty03@gmail.com>
2023-07-12 18:18:29 +0300

    mealApp

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar style='light'/>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+                headerTintColor: 'white',
+                headerStyle:{ backgroundColor: '#351401', },
+                contentStyle:  {backgroundColor: '#3f2f25',}
+      }}>
+        <Stack.Screen name="MealsCategories" component={CategoriesScreen} 
+        options={{
+          title: 'All Categories',
+        }}/>
+        <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+    </>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {  
+
+  },  
 });
